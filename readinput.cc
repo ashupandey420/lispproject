5effa4810894974b097e2ad9731868b8141192a9
Ashutosh <ashutoshpandey620@gmail.com>
2017-02-27 20:41:54 -0500

    first completion

--- readinput.cc
+++ readinput.cc
@@ -24,7 +24,7 @@ public:
 };
 
 std::list <SExp*> SExp::idPointers;
-
+std::string error="Error: Invalid SExpression\n\n\n\n";
 void printSExp(SExp* s);
 SExp T("T");
 SExp NIL("NIL");
@@ -71,7 +71,7 @@ public:
 
 	}
 	void show(){
-		if(type==1){cout<<token<<'\n';}
+		if(type==1){cout<<token;}
 		else {printSExp(s);}
 	}
 
@@ -100,7 +100,7 @@ int main(){
 			string::iterator new_end = unique(input.begin(), input.end(), BothAreSpaces);
 			input.erase(new_end, input.end()); 
 	    	input.erase(remove(input.begin(), input.end(), '\n'),input.end());
-			
+			input.erase(remove(input.begin(), input.end(), '\t'),input.end());
 	    	parseInput(input);
 	    	
 	    	if (line == "$$"){
@@ -116,8 +116,7 @@ int main(){
 }
 
 void parseInput(string input){
-	cout<<"inside parse Input"<<"\n";
-	cout<<"input received: "<<input<<"\n\n\n";
+	cout<<"input received: "<<input<<"\n\n";
 	stack<stackNode*> myStack;
 	
 	string tempStr;
@@ -125,12 +124,10 @@ void parseInput(string input){
 	int tempInt;
 	std::string::iterator it=input.begin();
 	while(1){
-		
-		cout<<*it<<"\n";
 		if(it==input.end()){break;}
 		else if(*it==' '){++it;}
 		else if(*it=='(' || *it=='.'){
-			cout<<"jai na ho"<<"\n";
+			//cout<<"neha\n";
 			stackNode* tempNode= new stackNode(*it);
 			myStack.push(tempNode);
 			++it;
@@ -143,25 +140,21 @@ void parseInput(string input){
 				tempStr+=*it;
 				++it;
 			}
-			if(*it!='('&& *it!=')' && *it!='.' && *it!=' '){
-				cout<<"error: illegal character "<<*it<<" in string.\n";
-				exit(1);
+			if(*it!='('&& *it!=')' && *it!='.' && *it!=' '&& it!=input.end()){
+				cout<<"Error: Illegal character "<<*it<<" in string.\n\n\n\n";
+				return;
 			}
 			if(!isvalidID(tempStr)){
-				cout<<"error: Illegal identifeir "<<tempStr<<" in string.\n";
-				exit(1);
+				cout<<"Error: Illegal identifeir "<<tempStr<<" in string.\n\n\n\n";
+				return;
 			}
-			cout<<"Identifier is: "<<tempStr<<"\n";
 			if(isInteger(tempStr)){
-				cout<<tempStr<<"  is  integer";
 				tempInt=std::stoi(tempStr);
-				cout<<tempInt<<"\n";
 				SExp* s =new SExp(tempInt);
 				stackNode* tempNode= new stackNode(s);
 				myStack.push(tempNode);
 			}
 			else{
-				cout<<tempStr<<"  is  not integer";
 				std::pair<bool,SExp*> p;
 				p=doesExist(tempStr              );
 				if(!p.first){
@@ -179,309 +172,133 @@ void parseInput(string input){
 			
 			
 		}
-		
-		/*else if(*it==')'){
-			while(!myStack.empty()){
-				cout<<"shivam"<<"\n";
-				myStack.top()->show();
-				myStack.pop();
-			}
-			std::list<SExp*>::iterator it;
-			for(it=SExp::idPointers.begin();it!=SExp::idPointers.end();++it){
-				(*it)->show();
-			}
-			exit(1);
-		}*/
 
 		else if(*it==')'){
+			//cout<<"inside )\n";
 			if(myStack.empty()){
-				cout<<"error: Invalid S expression\n";
-				exit(1);
+				cout<<error;
+				return;
 			}
-
-			cout<<"inside )"<<"\n";
-
 			stackNode* s1=myStack.top();
-
-			s1->show();
 			
 			if(s1->type==1){
-				
 				if(s1->token=='('){
-					cout<<"jai ho"<<"\n";
+					//cout<<"shivam\n";
 					myStack.pop();
-					myStack.top()->show();
 					stackNode* tempNode= new stackNode (&NIL);
 					myStack.push(tempNode);
 
 				}
 				else{
-					cout<<"error: Invalid Sexpression\n";
-					exit(1);
+					cout<<error;
+					return;
 				}
 			}
 			else if(s1->type==2){
 				myStack.pop();
+				if(myStack.empty()){
+					cout<<error;
+					return;
+				}
 				stackNode *s2=myStack.top();
 				if(s2->type==1 and s2->token==')'){
-					cout<<"error: Invalid Sexpression";
-					exit(1);
+					cout<<error;
+					return;
 				}
 				else if(s2->type==1 and s2->token=='('){
 					myStack.pop();
-					myStack.top()->show();
 					SExp* temp=new SExp(s1->s,&NIL);
 					stackNode* t=new stackNode(temp);
 					myStack.push(t);
 				}
 				else if(s2->type==1 and s2->token=='.'){
 					myStack.pop();
+					if(myStack.empty()){
+						cout<<error;
+						return;
+					}
 					stackNode* s3=myStack.top();
 					if(myStack.top()->type==1){
-						cout<<"error: Invalid Sexpression\n";
-						exit(1);
+						cout<<error;
+						return;
 					}
 					else{
 						myStack.pop();
+						if(myStack.empty()){
+							cout<<error;
+							return;
+						}
 						if(!(myStack.top()->type==1 and myStack.top()->token=='(')){
-							cout<<"error: Invalid Sexpression\n";
-							exit(1);
+							cout<<error;
+							return;
 						}
 						else{
-							myStack.top()->show();
+
+							myStack.pop();
 							SExp* temp=new SExp(s3->s,s1->s);
 							stackNode* t=new stackNode(temp);
 							myStack.push(t);
 						}
 					}
 				}
-				
-			++it;
-		}
-	}
-	myStack.top()->show();
-}			/*else if(s1->type==2){
-	}			myStack.pop();
-				cout<<"bhako";
-				myStack.top()->show();
-				if(myStack.empty()){
-					cout<<"error: Invalid S expression\n";
-					exit(1);
-				}
-				stackNode* s2=myStack.top();
-				cout<<"bhak";
-				myStack.top()->show();
-				if (s2->type==2 or s2->type==3){
-					SExp* r=&NIL;
-					while(!(myStack.top()->type==1) and !myStack.empty()){
-						stackNode* temp=myStack.top();
-						SExp *l;
-						if(temp->type==2){
-							SExp* l1= new SExp(temp->value);
-							l=l1;
-						}
-						else if(temp->type==3){
-							SExp* l1 = new SExp(temp->id);
-							l=l1;
-						}
-						else if(temp->type==4){
-							SExp* l1 = new SExp(*(temp->s));
-							l=l1;
-						}
-						r=CONS(l,r);
+				else if(s2->type==2){
+					SExp* r=s1->s;
+					r=new SExp(r,&NIL);
+					while(!(myStack.empty()) and !(myStack.top()->type==1)){
+						r=new SExp(myStack.top()->s,r);
 						myStack.pop();
 					}
 					if(myStack.empty()){
-						cout<<"error: Invalid S expression\n";
-						exit(1);
+						cout<<error;
+						return;
 					}
-					if(myStack.top()->token!='('){
-						cout<<"error: Invalid S expression\n";
-						exit(1);
-					}
-					myStack.pop();
-					stackNode* t= new stackNode(r);
-					myStack.push(t);
-				}
-
-
-			}*/
-			
-			
-			
-		
-
-	
-	/*while(!myStack.empty()){
-		cout<<"shivam"<<"\n";
-			myStack.top()->show();
-			myStack.pop();
-		}
-		exit(1);*/
-
-
-	
-		
-			/*else if(s1->type==2){	
-				
-				if(myStack.top()->type==1){
-
-					if(myStack.top()->token=='('){
-						cout<<"kuttu\n";
-						myStack.pop();
-						SExp temp(s1->value);
-						myStack temp1(CONS(&temp,&NIL));
-						myStack.push(&temp1);
-						myStack.top()->show();
-					}
-					else if(myStack.top()->token=='.'){
-						cout<<"shivam"<<"\n";
-						myStack.pop();
-						if(myStack.top()->type==2){	
-							stackNode* n1=myStack.top();
-							myStack.pop();
-							if(myStack.top()->type==1){
-								if(myStack.top()->token=='('){
-									myStack.pop();
-
-									SExp templ(n1->value);
-									SExp tempr(s1->value);
-									stackNode temp(CONS(&templ,&tempr));
-									myStack.push(&temp);
-									cout<<"neha"<<"\n";
-									myStack.top()->show();
-									
-								}
-								else{
-									cout<<"error: Invalid Sexpression\n";
-									exit(1);
-								}
-							}
-							else{
-								cout<<"error: Invalid Sexpression\n";
-								exit(1);
-							}
-
-						}
-						else if(myStack.top()->type==3){
-							stackNode* n1=myStack.top();
-							myStack.pop();
-							if(myStack.top()->type==1){
-								if(myStack.top()->token=='('){
-									myStack.pop();
-
-									SExp templ(n1->id);
-									SExp tempr(s1->value);
-									stackNode temp(CONS(&templ,&tempr));
-									myStack.push(&temp);
-									cout<<"neha"<<"\n";
-									myStack.top()->show();
-									
-									
-								}
-								else{
-									cout<<"error: Invalid Sexpression\n";
-								}
-							}
-							else{
-								cout<<"error: Invalid Sexpression\n";
-								exit(1);
-							}
-						}
-						else if(myStack.top()->type==4){
-							cout<<"inside 4";
-							
-							stackNode* n1=myStack.top();
-							
-							n1->show();
-							exit(1);
-							//n1.show();
-							
-							myStack.pop();
-							if(myStack.top()->type==1){
-								myStack.top()->show();
-								cout<<"bakchodi"<<"\n";
-								
-								cout<<myStack.top()->token<<"\n";
-
-								if(myStack.top()->token=='('){
-
-									cout<<"kuttu"<<"\n";
-									myStack.pop();
-									SExp temp(s1->value);
-									temp.show();
-									n1->s->show();
-									SExp* t=CONS(n1->s,&temp);
-									t->show();
-									exit(1);
-									myStack temp1(CONS(n1->s,&temp));
-									myStack.push(&temp1);
-									myStack.top()->show();
-									
-									
-									
-								}
-								else{
-									cout<<"error: Invalid Sexpression\n";
-									exit(1);
-								}
-							}
-							else{
-								cout<<"error: Invalid Sexpression\n";
-								exit(1);
-							}
-						}
-					}
-					
-
-				}
-
-				/*else if(myStack.top().type==2||myStack.top().type==3){
-					SExp r(s.value);
-					SExp* r1=CONS(&r,&NIL);
-					while(!(myStack.top().type==1 && myStack.top().token=='(')&& !myStack.empty()){
-						stackNode n1=myStack.top();
-						
-						if(n1.type==2){
-							SExp l(n1.value);
-							r1=CONS(&l,r1);
-						}
-						else if(n1.type==3){
-							SExp l(n1.id);
-							r1=CONS(&l,r1);
-						}
-						else if(n1.type==4){
-							r1=CONS(n1.s,r1);
-						}
-						else{
-							cout<<"error: Invalid Sexpression\n";
-							exit(1);
-						}
-						myStack.pop();
-					}
-					if(myStack.empty()){
-						cout<<"error: Invalid Sexpression\n";
+					else if(myStack.top()->token=='.' or myStack.top()->token==')'){
+						cout<<error;
+						return;
 					}
 					else{
 						myStack.pop();
-						stackNode t(r1);
+						stackNode* t=new stackNode(r);
 						myStack.push(t);
-						++it;
 					}
 				}
 			}
-		//else if(*it)*/
-			/*cout<<"end ) \n";
 			++it;
-		
-			}
-		}*/
-		
+			//cout<<"out of it\n";
+		}
 
-	
-	/*if(input[0]=='('){
-		cout<<'('<<"\n";
+	}
+	if(myStack.empty()){
+		cout<<error;
+	}
+	else{
+		stackNode *s1=myStack.top();
+		if(s1->type!=2){
+			cout<<error;
+			return;
+		}
+		myStack.pop();
+		if(!myStack.empty()){
+			cout<<error;
+			return;
+		}
+		s1->show();
+		cout<<"\n\n\n\n";
+	}
+	/*while(!myStack.empty()){
+		myStack.top()->show();
+		cout<<"   "<<"\n";
+		myStack.pop();
+	}
+	cout<<"printting idPointers\n";
+	std::list<SExp*>::iterator it1;
+	for(it1=SExp::idPointers.begin();it1!=SExp::idPointers.end();++it1){
+		(*it1)->show();
+		cout<<"   "<<"\n";
 	}*/
+	return;
+}			
+	
 
 
 inline bool isInteger(const std::string & s)
